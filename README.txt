3e59fa9d4a6292d871850087f4943579b1bfa158
rzr911 <chelseabluerockers@gmail.com>
2013-04-15 20:29:35 +0530

    Square root finder.

---
+++ README.txt
@@ -0,0 +1,2 @@
+This script finds out the square root of a number. But I need help creating a new else statement. I want to print out a error when an alphabet or any other
+character is typed. Can anyone help me?
---
+++ Square root.py
@@ -0,0 +1,9 @@
+print "Welcome to rzr911's square root finder!"
+import math
+number=raw_input("Enter the number you wish to find its square root: ")
+word=number
+word=float(word) 
+if type(word)==type(4.2):
+    print math.sqrt(word)
+
+raw_input("Press Enter to quit")
